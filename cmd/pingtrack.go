af1bfb047b17ee0fa7a3baef7d557c7bfa2dca60
Eric Anderson <anderson@ttel.com>
2020-02-16 16:13:36 -0600

    Add simple host API

--- cmd/pingtrack.go
+++ cmd/pingtrack.go
@@ -103,19 +103,25 @@ func DataHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// spaHandler implements the http.Handler interface, so we can use it
-// to respond to HTTP requests. The path to the static directory and
-// path to the index file within that static directory are used to
-// serve the SPA in the given static directory.
+func HostsHandler(w http.ResponseWriter, r *http.Request) {
+	keys := []string{}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	for k := range data.Results {
+		keys = append(keys, k)
+	}
+
+	jsonResults, _ := json.Marshal(keys)
+	w.Write(jsonResults)
+}
+
 type spaHandler struct {
 	staticPath string
 	indexPath  string
 }
 
-// ServeHTTP inspects the URL path to locate a file within the static dir
-// on the SPA handler. If a file is found, it will be served. If not, the
-// file located at the index path on the SPA handler will be served. This
-// is suitable behavior for serving an SPA (single page application).
 func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// get the absolute path to prevent directory traversal
 	path, err := filepath.Abs(r.URL.Path)
@@ -129,29 +135,11 @@ func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// prepend the path with the path to the static directory
 	path = filepath.Join(h.staticPath, path)
 
-	// check whether a file exists at the given path
-	/*
-	_, err = os.Stat(path)
-	if os.IsNotExist(err) {
-		// file does not exist, serve index.html
-		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
-		return
-	} else if err != nil {
-		// if we got an error (that wasn't that the file doesn't exist) stating the
-		// file, return a 500 internal server error and stop
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	 */
-
 	statikFS, err := fs.New()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// otherwise, use http.FileServer to serve the static dir
-	//http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 	http.FileServer(statikFS).ServeHTTP(w, r)
 }
 
@@ -196,6 +184,7 @@ func main() {
 	resq := make(chan Results)
 
 	r := mux.NewRouter()
+	r.HandleFunc("/api/hosts/", HostsHandler)
 	r.HandleFunc("/api/data/", DataHandler)
 	r.HandleFunc("/api/data/{hostname}/", DataHandler)
 
