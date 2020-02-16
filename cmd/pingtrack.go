1deaebb4a55ea419526793cffea05ea11df6f53e
Eric Anderson <anderson@ttel.com>
2020-02-16 16:20:30 -0600

    Organize

--- cmd/pingtrack.go
+++ cmd/pingtrack.go
@@ -1,25 +1,18 @@
 package main
 
-//go:generate statik -f -src=../web/public/ -dest=./internal/
-
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
-	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/rakyll/statik/fs"
 	"github.com/sparrc/go-ping"
 	"github.com/spf13/cobra"
-
-	_ "github.com/destari/pingtrack/cmd/internal/statik"
 )
 
 type Results struct {
@@ -88,59 +81,6 @@ func pinger(queue chan string, output chan Results) {
 			output <- res
 		}
 	}
-}
-
-func DataHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	if vars["hostname"] != "" {
-		jsonResults, _ := json.Marshal(data.Results[vars["hostname"]])
-		w.Write(jsonResults)
-	} else {
-		jsonResults, _ := json.Marshal(data)
-		w.Write(jsonResults)
-	}
-}
-
-func HostsHandler(w http.ResponseWriter, r *http.Request) {
-	keys := []string{}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	for k := range data.Results {
-		keys = append(keys, k)
-	}
-
-	jsonResults, _ := json.Marshal(keys)
-	w.Write(jsonResults)
-}
-
-type spaHandler struct {
-	staticPath string
-	indexPath  string
-}
-
-func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// get the absolute path to prevent directory traversal
-	path, err := filepath.Abs(r.URL.Path)
-	if err != nil {
-		// if we failed to get the absolute path respond with a 400 bad request
-		// and stop
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	// prepend the path with the path to the static directory
-	path = filepath.Join(h.staticPath, path)
-
-	statikFS, err := fs.New()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	http.FileServer(statikFS).ServeHTTP(w, r)
 }
 
 var data Data
